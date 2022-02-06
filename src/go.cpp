f86c31d6b7dad4b34f3f2260dcd247892da88bf0
dellevelled <delle.caj@gmail.com>
2022-02-06 18:46:13 -0500

    Add ko rule checking to placement validity; Condense IsValidPlacement(); Extract piece placement into PlacePiece()

--- src/go.cpp
+++ src/go.cpp
@@ -1,7 +1,7 @@
-//TODO ko rule when only one stone is captured (and optional superko)
 //TODO passing turn
 //TODO game end
 //TODO komi = 6.5
+//TODO maybe superko?
 
 //// @includes ////
 #include "deshi.h"
@@ -16,15 +16,10 @@ enum{
 };
 
 struct Chain{ //TODO make this one array where negative numbers mean inner or outer
-	array<u32> outer = array<u32>(deshi_allocator); //indexes of slots with liberties (can't be empty)
-	array<u32> inner = array<u32>(deshi_allocator); //indexes of slots with no liberties b/c of friendly neighbours (can be empty)
-};
-
-struct Board{
-	vec2 pos; //draw position
-	vec2 dims; //draw dimensions
-	u32  size; //number of rows/cols
-	carray<u32> slots; //row major matrix
+	array<u32> outer; //indexes of slots with liberties (can't be empty)
+	array<u32> inner; //indexes of slots with no liberties b/c of friendly neighbours (can be empty)
+	
+	Chain(Allocator* a = deshi_allocator) : outer(a), inner(a) {}
 };
 
 //// @variables ////
@@ -32,47 +27,63 @@ const char* board_letters[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"
 const char* board_numbers[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", };
 const char* board_numbers_spacing[] = { " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", };
 Font* font;
+vec2 board_pos;
+vec2 board_dims;
+u32  board_size;
 b32 suicide_is_illegal = true;
 
-Board board;
 b32 black_turn = true;
 b32 turn_passed = false;
+u32 whites_prev_placement = -1;
+u32 blacks_prev_placement = -1;
+u32 whites_prev_capture = -1; //value is -1 if more than one was captured
+u32 blacks_prev_capture = -1;
+u32 whites_capture_count = 0;
+u32 blacks_capture_count = 0;
+
+carray<u32>  slots; //row major matrix
+carray<u32>  chained; //chain indexes of slots
 array<Chain> chains(deshi_allocator);
-array<u32>  chained(deshi_allocator);
 
 //// @functions ////
-#define ToLinear(row,col) ((board.size*(row))+(col))
-#define SlotLinear(pos) board.slots[pos]
+#define ToLinear(row,col) ((board_size*(row))+(col))
 
-#define Slot(row,col) board.slots[ToLinear(row,col)]
+#define SlotLinear(pos) slots[pos]
+#define Slot(row,col) slots[ToLinear(row,col)]
 #define SlotIsColor(row,col,color) (Slot(row,col) == color)
 #define SlotIsEmpty(row,col) SlotIsColor(row,col,Slot_Empty)
 #define SlotIsWhite(row,col) SlotIsColor(row,col,Slot_White)
 #define SlotIsBlack(row,col) SlotIsColor(row,col,Slot_Black)
 
+#define ChainAtLinear(pos) chains[pos]
+#define ChainAt(row,col) chains[ToLinear(row,col)]
+#define ChainedLinear(pos) chained[pos]
+#define Chained(row,col) chained[ToLinear(row,col)]
+#define ChainCount(row,col) (chains[Chained(row,col)].inner.count + chains[Chained(row,col)].outer.count)
+
 //returns false if already chained or a different color
-b32 FillChain(Chain* chain, u32 row, u32 col, u32 color){
+b32 FillChain(u32 chain_idx, u32 row, u32 col, u32 color){
 	u32 pos = ToLinear(row,col);
 	
 	//exit if not chainable
 	if(SlotLinear(pos) != color) return false;
-	forI(chained.count){ if(pos == chained[i]){ return false; } }
+	if(ChainedLinear(pos) != -1) return false;
 	
-	//add to chained list
-	chained.add(pos);
+	//set to this chain
+	ChainedLinear(pos) = chain_idx;
 	
 	//check neighbours
 	u32 neighbour_count = 0;
-	if(col > 0            && FillChain(chain,row,  col-1,color)){ neighbour_count++; } //LEFT
-	if(row > 0            && FillChain(chain,row-1,col  ,color)){ neighbour_count++; } //UP
-	if(col < board.size-1 && FillChain(chain,row,  col+1,color)){ neighbour_count++; } //RIGHT
-	if(row < board.size-1 && FillChain(chain,row+1,col  ,color)){ neighbour_count++; } //DOWN
+	if(col > 0            && FillChain(chain_idx,row,  col-1,color)){ neighbour_count++; } //LEFT
+	if(row > 0            && FillChain(chain_idx,row-1,col  ,color)){ neighbour_count++; } //UP
+	if(col < board_size-1 && FillChain(chain_idx,row,  col+1,color)){ neighbour_count++; } //RIGHT
+	if(row < board_size-1 && FillChain(chain_idx,row+1,col  ,color)){ neighbour_count++; } //DOWN
 	
 	//check neighbour count to see if inner or outer part of chain
 	if(neighbour_count == 4){
-		chain->inner.add(pos);
+		chains[chain_idx].inner.add(pos);
 	}else{
-		chain->outer.add(pos);
+		chains[chain_idx].outer.add(pos);
 	}
 	
 	return true;
@@ -82,107 +93,121 @@ u32 LibertyCount(u32 row, u32 col){
 	u32 count = 0;
 	if(row > 0            && SlotIsEmpty(row-1,col)) count++;
 	if(col > 0            && SlotIsEmpty(row,col-1)) count++;
-	if(row < board.size-1 && SlotIsEmpty(row+1,col)) count++;
-	if(col < board.size-1 && SlotIsEmpty(row,col+1)) count++;
+	if(row < board_size-1 && SlotIsEmpty(row+1,col)) count++;
+	if(col < board_size-1 && SlotIsEmpty(row,col+1)) count++;
 	return count;
 }
 u32 LibertyCount(Chain* chain){
 	u32 count = 0;
-	forE(chain->outer){ count += LibertyCount(*it / board.size, *it % board.size); }
+	forE(chain->outer){ count += LibertyCount(*it / board_size, *it % board_size); }
 	return count;
 }
 
 b32 IsValidPlacement(u32 row, u32 col){
-	//first, return true if placement will capture something
-	//then,  return false check if placement is suicidal (if suicide is not allowed)
-	u32 color = (black_turn) ? Slot_White : Slot_Black;
-	if(row == 0){
-		if(col == 0){                  //TOP LEFT
-			if(   (SlotIsColor(row+1,col,color) && LibertyCount(row+1,col) == 1)
-			   || (SlotIsColor(row,col+1,color) && LibertyCount(row,col+1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row+1,col,color)
-			   && SlotIsColor(row,col+1,color)) return false;
-		}else if(col == board.size-1){ //TOP RIGHT
-			if(   (SlotIsColor(row+1,col,color) && LibertyCount(row+1,col) == 1)
-			   || (SlotIsColor(row,col-1,color) && LibertyCount(row,col-1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row+1,col,color)
-			   && SlotIsColor(row,col-1,color)) return false;
-		}else{                         //TOP EDGE
-			if(   (SlotIsColor(row+1,col,color) && LibertyCount(row+1,col) == 1)
-			   || (SlotIsColor(row,col+1,color) && LibertyCount(row,col+1) == 1)
-			   || (SlotIsColor(row,col+1,color) && LibertyCount(row,col+1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row+1,col,color)
-			   && SlotIsColor(row,col+1,color)
-			   && SlotIsColor(row,col-1,color)) return false;
-		}
-	}else if(row == board.size-1){
-		if(col == 0){                  //BOT LEFT
-			if(   (SlotIsColor(row-1,col,color) && LibertyCount(row-1,col) == 1)
-			   || (SlotIsColor(row,col+1,color) && LibertyCount(row,col+1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row-1,col,color)
-			   && SlotIsColor(row,col+1,color)) return false;
-		}else if(col == board.size-1){ //BOT RIGHT
-			if(   (SlotIsColor(row-1,col,color) && LibertyCount(row-1,col) == 1)
-			   || (SlotIsColor(row,col-1,color) && LibertyCount(row,col-1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row-1,col,color)
-			   && SlotIsColor(row,col-1,color)) return false;
-		}else{                         //BOT EDGE
-			if(   (SlotIsColor(row-1,col,color) && LibertyCount(row-1,col) == 1)
-			   || (SlotIsColor(row,col+1,color) && LibertyCount(row,col+1) == 1)
-			   || (SlotIsColor(row,col-1,color) && LibertyCount(row,col-1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row-1,col,color)
-			   && SlotIsColor(row,col+1,color)
-			   && SlotIsColor(row,col-1,color)) return false;
-		}
-	}else{
-		if(col == 0){                  //LEFT EDGE
-			if(   (SlotIsColor(row-1,col,color) && LibertyCount(row-1,col) == 1)
-			   || (SlotIsColor(row+1,col,color) && LibertyCount(row+1,col) == 1)
-			   || (SlotIsColor(row,col+1,color) && LibertyCount(row,col+1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row-1,col,color)
-			   && SlotIsColor(row+1,col,color)
-			   && SlotIsColor(row,col+1,color)) return false;
-		}else if(col == board.size-1){ //RIGHT EDGE
-			if(   (SlotIsColor(row-1,col,color) && LibertyCount(row-1,col) == 1)
-			   || (SlotIsColor(row+1,col,color) && LibertyCount(row+1,col) == 1)
-			   || (SlotIsColor(row,col-1,color) && LibertyCount(row,col-1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row-1,col,color)
-			   && SlotIsColor(row+1,col,color)
-			   && SlotIsColor(row,col-1,color)) return false;
-		}else{                         //NON EDGE OR CORNER
-			if(   (SlotIsColor(row-1,col,color) && LibertyCount(row-1,col) == 1)
-			   || (SlotIsColor(row+1,col,color) && LibertyCount(row+1,col) == 1)
-			   || (SlotIsColor(row,col-1,color) && LibertyCount(row,col-1) == 1)
-			   || (SlotIsColor(row,col+1,color) && LibertyCount(row,col+1) == 1)) return true;
-			if(   suicide_is_illegal
-			   && SlotIsColor(row-1,col,color)
-			   && SlotIsColor(row+1,col,color)
-			   && SlotIsColor(row,col-1,color)
-			   && SlotIsColor(row,col+1,color)) return false;
-		}
-	}
+	u32 color = (black_turn) ? Slot_White : Slot_Black; //NOTE non-active player's color
+	u32 pos = ToLinear(row,col);
+	
+	//find how many captures would be made and whether stone will be surrounded if placed at slot
+#define CheckNeighbor(row,col) { if(SlotIsColor(row,col,color)){ if(LibertyCount(&chains[Chained(row,col)]) == 1){ captures += ChainCount(row,col); } }else{ surrounded = false; } }
+	u32 captures = 0;
+	b32 surrounded = true;
+	if(row > 0) CheckNeighbor(row-1,col);
+	if(col > 0) CheckNeighbor(row,col-1);
+	if(row < board_size-1) CheckNeighbor(row+1,col);
+	if(col < board_size-1) CheckNeighbor(row,col+1);
+#undef CheckNeighbor
+	
+	//ko rule is violated (and placement is invalid) if:
+	//    placement will only capture one stone
+	//    that stone was played on the previous player's move
+	//    and that move only captured one stone
+	if(captures == 1
+	   && ((black_turn) ? whites_prev_capture   : blacks_prev_capture)   != -1
+	   && ((black_turn) ? whites_prev_placement : blacks_prev_placement) != pos) return false;
+	
+	//if placement will captures something and we dont violate ko rule, placement is valid
+	if(captures > 0) return true;
+	
+	//if placement wont capture anything and is suicidal by being surrounded by enemies, placement is invalid
+	if(surrounded) return false;
+	
 	return true;
 }
 
+void PlacePiece(u32 row, u32 col){
+	//update slot
+	Slot(row,col) = (black_turn) ? Slot_Black : Slot_White;
+	
+	//remake chains
+	clear(chained, 0xFF);
+	chains.clear();
+	forX(col, board_size){
+		forX(row, board_size){
+			chains.add(Chain());
+			if(SlotIsEmpty(row,col) || !FillChain(chains.count-1, row, col, Slot(row,col))) chains.pop();
+		}
+	}
+	
+	//check for chain liberties on enemy pieces (NOTE enemy before friendly so capturing has precedence over suicide)
+	u32 enemy = (black_turn) ? Slot_White : Slot_Black;
+	u32 prev_capture_index = -1;
+	u32 capture_count = 0;
+	forI(chains.count){
+		if(SlotLinear(chains[i].outer[0]) != enemy) continue;
+		if(LibertyCount(&chains[i]) == 0){
+			forE(chains[i].inner){ SlotLinear(*it) = Slot_Empty; prev_capture_index = (capture_count) ? -1 : *it; capture_count++; ChainedLinear(*it) = -1; }
+			forE(chains[i].outer){ SlotLinear(*it) = Slot_Empty; prev_capture_index = (capture_count) ? -1 : *it; capture_count++; ChainedLinear(*it) = -1; }
+			forE(chained){ if(*it != -1 && *it >= i){ *it -= 1; } }
+			chains.remove(i--);
+		}
+	}
+	
+	//check for chain liberties on friendly pieces if suicide is legal
+	u32 friendly = (black_turn) ? Slot_Black : Slot_White;
+	u32 other_capture_count = 0;
+	if(!suicide_is_illegal){
+		forI(chains.count){
+			if(SlotLinear(chains[i].outer[0]) != friendly) continue;
+			if(LibertyCount(&chains[i]) == 0){
+				forE(chains[i].inner){ SlotLinear(*it) = Slot_Empty; other_capture_count++; }
+				forE(chains[i].outer){ SlotLinear(*it) = Slot_Empty; other_capture_count++; }
+				forE(chained){ if(*it == i) *it = -1; }
+				chains.remove(i--);
+			}
+		}
+	}
+	
+	//update prev capture/placement and capture counts
+	if(black_turn){
+		blacks_prev_placement = ToLinear(row, col);
+		blacks_prev_capture   = prev_capture_index;
+		blacks_capture_count += capture_count;
+		whites_capture_count += other_capture_count;
+	}else{
+		whites_prev_placement = ToLinear(row, col);
+		whites_prev_capture   = prev_capture_index;
+		whites_capture_count += capture_count;
+		blacks_capture_count += other_capture_count;
+	}
+	
+	//swap turns
+	ToggleBool(black_turn);
+}
+
+void AddGoCommands();
 int main(int argc, char* argv[]){ //NOTE argv includes the entire command line (including .exe)
 	//// @init ////
 	deshi_init();
 	
 	font = Storage::CreateFontFromFileTTF("STIXTwoMath-Regular.otf", 48).second;
 	
+	board_size = 9;
 	f32 board_width = (DeshWindow->width < DeshWindow->height) ? DeshWindow->width/2 : DeshWindow->height/2;
-	board.pos = (DeshWindow->dimensions-vec2{board_width,board_width})/2;
-	board.dims.set(board_width, board_width);
-	board.size = 9;
-	init(&board.slots, board.size*board.size, deshi_allocator);
+	board_pos = (DeshWindow->dimensions-vec2{board_width,board_width})/2;
+	board_dims.set(board_width, board_width);
+	init(&slots, board_size*board_size, deshi_allocator);
+	init(&chained, slots.count, deshi_allocator);
+	AddGoCommands();
 	
 	DeshWindow->UpdateDecorations(Decoration_SystemDecorations);
 	
@@ -191,48 +216,46 @@ int main(int argc, char* argv[]){ //NOTE argv includes the entire command line (
 	//UI::ShowMetricsWindow();
 	UI::Begin("go", vec2::ZERO, DeshWindow->dimensions, UIWindowFlags_NoInteract | UIWindowFlags_Invisible);
 	
-	
-	f32 row_gap = board.dims.x / (board.size-1);
+	f32 row_gap = board_dims.x / (board_size-1);
 	f32 piece_radius = row_gap/2;
 	vec2 border{row_gap, row_gap*3};
 	b32 piece_placed = false;
 	
 	//draw background
-	UI::RectFilled(board.pos-border, board.dims+(2*border), color(220,179,92));
+	UI::RectFilled(board_pos-border, board_dims+(2*border), color(220,179,92));
 	
 	//draw grid and border text
 	UI::PushVar(UIStyleVar_FontHeight, row_gap/2); UI::PushFont(font); UI::PushColor(UIStyleCol_Text, Color_Black);
-	forI(board.size){
+	forI(board_size){
 		f32 offset = i*row_gap;
-		UI::Line(board.pos+vec2{offset,0}, board.pos+vec2{offset,board.dims.x}, 1, Color_Black);
-		UI::Line(board.pos+vec2{0,offset}, board.pos+vec2{board.dims.x,offset}, 1, Color_Black);
-		UI::Text(board_letters[i], board.pos+vec2{offset-row_gap/6,-row_gap});
-		UI::Text(board_letters[i], board.pos+vec2{offset-row_gap/6,board.dims.x+row_gap/2});
-		UI::Text(board_numbers_spacing[board.size-i-1], board.pos+vec2{-row_gap,offset-row_gap/4});
-		UI::Text(board_numbers_spacing[board.size-i-1], board.pos+vec2{board.dims.x+row_gap/2,offset-row_gap/4});
+		UI::Line(board_pos+vec2{offset,0}, board_pos+vec2{offset,board_dims.x}, 1, Color_Black);
+		UI::Line(board_pos+vec2{0,offset}, board_pos+vec2{board_dims.x,offset}, 1, Color_Black);
+		UI::Text(board_letters[i], board_pos+vec2{offset-row_gap/6,-row_gap});
+		UI::Text(board_letters[i], board_pos+vec2{offset-row_gap/6,board_dims.x+row_gap/2});
+		UI::Text(board_numbers_spacing[board_size-i-1], board_pos+vec2{-row_gap,offset-row_gap/4});
+		UI::Text(board_numbers_spacing[board_size-i-1], board_pos+vec2{board_dims.x+row_gap/2,offset-row_gap/4});
 	}
 	UI::PopColor(); UI::PopFont(); UI::PopVar();
 	
 	//draw pieces, check for hovered pieces, and handle piece placement
 	u32 hovered_row = -1, hovered_col = -1;
-	forX(col, board.size){
-		forX(row, board.size){
+	forX(col, board_size){
+		forX(row, board_size){
 			vec2 offset{row*row_gap,col*row_gap};
-			vec2 intersection = board.pos+offset;
+			vec2 intersection = board_pos+offset;
 			if      (SlotIsWhite(row,col)){
 				UI::CircleFilled(intersection, piece_radius, 16, Color_White);
-				if(DeshInput->mousePos.distanceTo(board.pos+offset) <= piece_radius){ hovered_row = row; hovered_col = col; }
+				if(DeshInput->mousePos.distanceTo(board_pos+offset) <= piece_radius){ hovered_row = row; hovered_col = col; }
 			}else if(SlotIsBlack(row,col)){
 				UI::CircleFilled(intersection, piece_radius, 16, Color_Black);
-				if(DeshInput->mousePos.distanceTo(board.pos+offset) <= piece_radius){ hovered_row = row; hovered_col = col; }
+				if(DeshInput->mousePos.distanceTo(board_pos+offset) <= piece_radius){ hovered_row = row; hovered_col = col; }
 			}else if(SlotIsEmpty(row,col)){
-				if(DeshInput->mousePos.distanceTo(board.pos+offset) <= piece_radius){
+				if(DeshInput->mousePos.distanceTo(board_pos+offset) <= piece_radius){
 					UI::CircleFilled(intersection, piece_radius, 16, (black_turn) ? color(0,0,0,128) : color(255,255,255,128));
 					hovered_row = row; hovered_col = col;
 					if(IsValidPlacement(row,col)){
 						if(DeshInput->KeyPressed(MouseButton::LEFT)){
-							Slot(row,col) = (black_turn) ? Slot_Black : Slot_White;
-							piece_placed = true;
+							PlacePiece(row, col);
 						}
 					}else{
 						f32 corner = piece_radius*0.70f;
@@ -244,60 +267,46 @@ int main(int argc, char* argv[]){ //NOTE argv includes the entire command line (
 		}
 	}
 	
-	//if a piece is placed, check for captures
-	//TODO extract piece placement from drawing into here
-	if(piece_placed){
-		//clear chains
-		chains.clear();
-		chained.clear();
-		
-		//remake chains
-		forX(col, board.size){
-			forX(row, board.size){
-				Chain chain;
-				if(!SlotIsEmpty(row,col) && FillChain(&chain, row, col, Slot(row,col))){
-					chains.add(chain);
-				}
-			}
-		}
-		
-		//check for chain liberties on enemy pieces (NOTE this is first so capturing enemies has precedence)
-		u32 enemy = (black_turn) ? Slot_White : Slot_Black;
-		forI(chains.count){
-			if(SlotLinear(chains[i].outer[0]) != enemy) continue;
-			if(LibertyCount(&chains[i]) == 0){
-				forE(chains[i].inner){ SlotLinear(*it) = Slot_Empty; }
-				forE(chains[i].outer){ SlotLinear(*it) = Slot_Empty; }
-				chains.remove(i--);
-			}
-		}
-		
-		//check for chain liberties on friendly pieces
-		u32 friendly = (black_turn) ? Slot_Black : Slot_White;
-		forI(chains.count){
-			if(SlotLinear(chains[i].outer[0]) != friendly) continue;
-			if(LibertyCount(&chains[i]) == 0){
-				forE(chains[i].inner){ SlotLinear(*it) = Slot_Empty; }
-				forE(chains[i].outer){ SlotLinear(*it) = Slot_Empty; }
-				chains.remove(i--);
-			}
-		}
-		
-		//swap turns
-		ToggleBool(black_turn);
-	}
-	
 #if 1 //draw chains
 	forI(chains.count){
 		UI::PushColor(UIStyleCol_Text, (SlotLinear(chains[i].outer[0]) == Slot_White) ? Color_Black : Color_White);
-		forE(chains[i].inner){ UI::Text(toStr(i).str, board.pos+vec2{(*it/board.size)*row_gap,(*it%board.size)*row_gap}); }
-		forE(chains[i].outer){ UI::Text(toStr(i).str, board.pos+vec2{(*it/board.size)*row_gap,(*it%board.size)*row_gap}); }
+		forE(chains[i].inner){ UI::Text(toStr(i).str, board_pos+vec2{(*it/board_size)*row_gap,(*it%board_size)*row_gap}); }
+		forE(chains[i].outer){ UI::Text(toStr(i).str, board_pos+vec2{(*it/board_size)*row_gap,(*it%board_size)*row_gap}); }
 		UI::PopColor();
 	}
 #endif
+	
+	//reset board if f5 pressed
+	if(DeshInput->KeyPressed(Key::F5)){
+		Cmd::Run("go_reset");
+	}
 	
 	UI::End();
 	deshi_loop_end();
 	deshi_cleanup();
 	return 0;
+}
+
+//// @commands ////
+void AddGoCommands(){
+	const char* last_cmd_desc;
+#define CMDSTART(name, desc) last_cmd_desc = desc; auto deshi__cmd__##name = [](array<cstring>& args) -> void
+#define CMDEND(name, ...) ; Cmd::Add(deshi__cmd__##name, #name, last_cmd_desc, {__VA_ARGS__});
+	
+	CMDSTART(go_reset, "Resets the board to default state."){
+		clear(slots);
+		clear(chained, 0xFF);
+		chains.clear();
+		black_turn = true;
+		turn_passed = false;
+		whites_prev_placement = -1;
+		blacks_prev_placement = -1;
+		whites_prev_capture = -1;
+		blacks_prev_capture = -1;
+		whites_capture_count = 0;
+		blacks_capture_count = 0;
+	}CMDEND(go_reset);
+	
+#undef CMDSTART
+#undef CMDEND
 }
